efe56f3b6098e0b3d6523bcf84827b386dfe2c51
maddularamkumar223 <maddularamkumar223@gmail.com>
2026-01-03 17:48:50 +0530

    Unfollow

--- src/component/redux/slice/userSlice.js
+++ src/component/redux/slice/userSlice.js
@@ -1,14 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { follow_followers } from "../thunk/userThunk";
+import { follow_followers, unFollow_followers } from "../thunk/userThunk";
 
 let userSlice = createSlice({
   name: "userSlice",
-  initialState: { userFollowingList: "" },
+  initialState: { userFollowingList: "", isFollowersUpdate: false },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(follow_followers.fulfilled, (state, action) => {
-      state.userFollowingList = action.payload;
-    });
+    builder
+      .addCase(follow_followers.fulfilled, (state, action) => {
+        state.userFollowingList = action.payload;
+        state.isFollowersUpdate = !state.isFollowersUpdate;
+      })
+      .addCase(unFollow_followers.fulfilled, (state) => {
+        state.isFollowersUpdate = !state.isFollowersUpdate;
+      });
   },
 });
 
